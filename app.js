4f28d840e525f3094fb43f097e997b6cb019b117
anastasiiaderkach1996 <lgg16@me.com>
2019-11-26 16:29:43 +0200

    renamed

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+let db; 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+MongoClient.connect('mongodb://localhost:27017',
+    { useUnifiedTopology: true },
+    function(err,database){
+        if (err) {
+            console.log(err);
+        } 
+    app.listen(3012, function(err, database) {
+        console.log('API started')
+    });
+});
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {root: __dirname + '/public'});
+    console.log(__dirname);
+});
--- public/main.js
+++ public/main.js
@@ -16,6 +16,19 @@ toggleHeaderFixed()
 
 
 
+function sendAjax() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(xhttp.readyState == 4 && xhttp.status == 200) {
+            console.log(xhttp.responseText)
+        };
+    };
+    xhttp.open('GET', '/',true);
+    xhttp.setRequestHeader('Content-Type', 'application/json');
+    xhttp.send()
+}
+
+
 
 
 
